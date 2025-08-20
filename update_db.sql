446be4c34ba7c1720cca04a5a291068ae608e971
Yuwei Le <leyuwei126@126.com>
2025-08-20 15:31:57 +0800

    Delete update_db.sql

--- update_db.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS reimbursement_announcement (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  content TEXT NOT NULL
-);
-INSERT INTO reimbursement_announcement (id, content) VALUES (1, '') ON DUPLICATE KEY UPDATE content=content;
